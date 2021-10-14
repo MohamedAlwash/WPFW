a100a9a30e34a8e55125fcd4a0879e1330649e32
Mohamed Alwash <18046533@student.hhs.nl>
2021-10-14 12:56:02 +0200

    Week 7 done

---
+++ 7 MVC 2/Migrations/20211014104946_migrations1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _7_MVC_2.Migrations
+{
+    public partial class migrations1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
