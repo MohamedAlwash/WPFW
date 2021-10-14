b66ee30a1b45b2503dcd846c81a7bd153e230dba
Mohamed Alwash <18046533@student.hhs.nl>
2021-10-14 12:31:59 +0200

    Bootstrap still not done

--- 2 Bootstrap/home.js
+++ 2 Bootstrap/home.js
@@ -1,23 +1,29 @@
 function f() {
-    let name = document.getElementById("fname").value;
-    var nameTag = document.createElement("p");
-    var nameText = document.createTextNode(name);
-    nameTag.appendChild(nameText);
-    var nameElement = document.getElementById("column-studie");
-    nameElement.appendChild(nameTag);
+    let name = document.getElementById("InputName").value;
+    let students = document.getElementById("InputStudents").value;
+    let email = document.getElementById("InputEmail").value;
 
+    // if (students > 1000) {
+    //     $('#exampleModalCenter').modal({
+    //         keyboard: true
+    //       });
+    // };
 
-    let students = document.getElementById("students").value;
-    var studentsTag = document.createElement("p");
-    var studentsText = document.createTextNode(students);
-    studentsTag.appendChild(studentsText);
-    var studentsElement = document.getElementById("column-beschrijving");
-    studentsElement.appendChild(studentsTag);
-
-    let email = document.getElementById("email").value;
-    var emailTag = document.createElement("p");
-    var emailText = document.createTextNode(email);
-    emailTag.appendChild(emailText);
-    var emailElement = document.getElementById("column-email");
-    emailElement.appendChild(emailTag);
+    document.getElementById('tbody').innerHTML += '<tr><td>' + name + '</td><td>' + students + '</td><td>' + email + '</td></tr>';
 }
+
+// if (alertTrigger) {
+//     alertTrigger.addEventListener('click', function () {
+//         alert('Nice, you triggered this alert message!', 'success')
+//     })
+// }
+
+// var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+// var alertTrigger = document.getElementById('liveAlertBtn')
+
+// function alert(message, type) {
+//     var wrapper = document.createElement('div')
+//     wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+//     alertPlaceholder.append(wrapper)
+// }
